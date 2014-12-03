d6baa4c44eaf1eff08f1c6b565eb5679a75e40b6
wwwins <jacky.huang@isobar.com>
2014-12-03 16:35:11 +0800

    Init import.

---
+++ TouchHelperPlus/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  TouchHelperPlus
+//
+//  Created by wwwins on 2014/12/3.
+//  Copyright (c) 2014年 isobar. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  var detailItem: AnyObject? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail: AnyObject = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
