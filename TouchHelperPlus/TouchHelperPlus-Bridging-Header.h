023344fc1303931692075a5cde3cd81406d87f0c
wwwins <jacky.huang@isobar.com>
2014-12-03 17:52:20 +0800

    Added a Bridging-Header file.

---
+++ TouchHelperPlus/TouchHelperPlus-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  TouchHelperPlus-Bridging-Header.h
+//  TouchHelperPlus
+//
+//  Created by wwwins on 2014/12/3.
+//  Copyright (c) 2014年 isobar. All rights reserved.
+//
+
+#ifndef TouchHelperPlus_TouchHelperPlus_Bridging_Header_h
+#define TouchHelperPlus_TouchHelperPlus_Bridging_Header_h
+
+#import "KAWebViewController.h"
+#import "KAWModalWebViewController.h"
+#import "KAWToolbarItems.h"
+
+#import "COSTouchVisualizerWindow.h"
+
+#endif
